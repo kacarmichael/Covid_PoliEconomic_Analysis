3f32674e6837f6060f1c358fde2174d7b33a5b37
Aaron Carmichael <48739702+kacarmichael@users.noreply.github.com>
2020-08-16 20:42:52 -0500

    Update TickerScrape.py

--- TickerScrape.py
+++ TickerScrape.py
@@ -4,14 +4,14 @@ import os
 
 import re
 
-os.chdir(r'C:\Users\Aaron\Google Drive\School Stuff\Summer 2020\Project')
+os.chdir(r'****')
 
 url = "https://finviz.com/screener.ashx"
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox(executable_path=r'C:\Users\Aaron\Desktop\Gecko\geckodriver.exe', log_path=r'C:\Users\Aaron\Desktop\Gecko\geckodriver.log')
+driver = webdriver.Firefox(executable_path=r'****', log_path=r'****')
 driver.set_page_load_timeout(600)
 
 driver.get(url)
@@ -71,41 +71,6 @@ for sector in sector_text:
 
 df.to_csv("stocksymbols_2.csv")
 
-"""
-test_df = pd.DataFrame(columns = ['symbol', 'company_name', 'sector', 'industry'])
-test_sector = "basicmaterials"
-#No pagination
-#test_industry = 'agriculturalinputs'
-#Pagination
-test_industry = 'specialtychemicals'
-test_r = 1
-test_url = "https://finviz.com/screener.ashx?v=111&f=geo_usa,ind_"+test_industry+",sec_"+test_sector+"&r="+str(test_r)
-driver.get(test_url)
-test_pagination = driver.find_element_by_css_selector("#screener-content > table > tbody > tr:nth-child(7)")
-#Detects if there is no pagination
-if len(test_pagination.find_elements_by_tag_name("b")) == 1:
-    print("No pagination detected")
-    test_url_num = 1
-elif len(test_pagination.find_elements_by_tag_name("a")) > 1:
-    test_num_pages = test_pagination.find_elements_by_tag_name("a")[-2].text
-    test_url_num = (int(test_num_pages)-1)+(20*int(test_num_pages)-1)
-while test_r <= test_url_num:
-    test_url = "https://finviz.com/screener.ashx?v=111&f=geo_usa,ind_"+test_industry+",sec_"+test_sector+"&r="+str(test_r)
-    driver.get(test_url)
-    table_body = driver.find_element_by_css_selector("#screener-content > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1)")
-    table_rows = table_body.find_elements_by_tag_name("tr")[1:]
-    for row in table_rows:
-        cells = row.find_elements_by_tag_name("td")
-        symbol = cells[1].text
-        company_name = cells[2].text
-        sector = test_sector
-        industry = test_industry
-        new_row = pd.DataFrame(data = {'symbol': symbol, 'company_name': company_name, 'sector': sector, 'industry': industry}, index=[0])
-        test_df = test_df.append(new_row)
-    test_r += 20
-"""   
-
-"""
 #Export symbols as a txt file
 data = pd.read_csv("stocksymbols_full.csv")
 
@@ -116,7 +81,7 @@ sectors = data.loc[data['sector'].isin(['healthcare', 'consumercyclical', 'commu
 symbols = sectors['symbol']
 
 symbols.to_csv('symbols.txt', index=False, sep='\n')
-"""
+
 
 
 
